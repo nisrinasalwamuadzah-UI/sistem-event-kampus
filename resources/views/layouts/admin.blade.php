60bd8d27febbffdbee2947f59c5c3b9c6285ac24
ginganomercy <rafly1955@gmail.com>
2026-07-22 06:31:09 +0700

    style: change dashboard sidebar color from deep slate to a premium dark blue (#1e3a8a) to align with login page aesthetics, and bump cache version to v4

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,7 @@
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
     
     <!-- Unified CSS -->
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}?v=3">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}?v=4">
     
     @yield('extra_css')
 </head>
